14725b01c4064c73b083ee190f42b01c42ab758b
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-06 21:20:26 +0600

    added task-6

--- 6 bus fare calculator.js
+++ 6 bus fare calculator.js
@@ -1,8 +1,20 @@
 /***
-
 Ticket fare Calculator
     - Children (age < 10): free
     - Students get a 50% discount
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+const age = 65;
+const isStudent = false;
+let ticketPrice = 800;
+if (age < 10) {
+  ticketPrice = "Free";
+} else if (isStudent) {
+  ticketPrice = ticketPrice * (50 / 100);
+} else if (age >= 60) {
+  ticketPrice = ticketPrice * (15 / 100);
+} else {
+  ticketPrice;
+}
+console.log(ticketPrice);
