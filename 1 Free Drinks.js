a2bce2885f347dba80782bb23c48f5b8736fa6ef
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-02-06 17:04:54 +0600

    added task-1

--- 1 Free Drinks.js
+++ 1 Free Drinks.js
@@ -4,3 +4,17 @@ Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+
+const burger = 500;
+let freeDrink;
+
+// if else condition
+/*
+if (burger > 500) {
+  console.log("free Coke");
+} else {
+  console.log("Coke: 30tk");
+} */
+
+// ternary operator
+freeDrink = burger > 500 ? console.log("free Coke") : console.log("Coke: 30tk");
